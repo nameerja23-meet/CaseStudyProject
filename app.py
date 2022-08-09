934da77c77ab4dce8b3ded99e813df24d560c5a1
nameerja23-meet <nemojabara@gmail.com>
2022-08-09 17:11:15 +0300

    Fixed & linked form to fb

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import session as login_session
 import pyrebase
 
 config={
-    "apiKey": "AIzaSyCSGTfHg1EzMQootZldniM4bVzl2q5bAqE",
-    "authDomain": "casestudyproje.firebaseapp.com",
-    "projectId": "casestudyproje",
-    "storageBucket": "casestudyproje.appspot.com",
-    "messagingSenderId": "475964335933",
-    "appId": "1:475964335933:web:a0eb145ccebac19be80754",
-    "measurementId": "G-MPPTM5CX0T",
-    "databaseURL" : "https://casestudyproje-default-rtdb.europe-west1.firebasedatabase.app/"
+	"apiKey": "AIzaSyCSGTfHg1EzMQootZldniM4bVzl2q5bAqE",
+	"authDomain": "casestudyproje.firebaseapp.com",
+	"databaseURL": "https://casestudyproje-default-rtdb.europe-west1.firebasedatabase.app",
+	"projectId": "casestudyproje",
+	"storageBucket": "casestudyproje.appspot.com",
+	"messagingSenderId": "475964335933",
+	"appId": "1:475964335933:web:a0eb145ccebac19be80754",
+	"measurementId": "G-MPPTM5CX0T"
 }
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -23,9 +23,27 @@ app.config['SECRET_KEY'] = 'fsoifj23423@#$sdfj#%#$)@48*(*)spwv@#$@#@2849238dfskj
 def index():
     return render_template('index.html')
 
-@app.route('/donate')
-def donate():
+@app.route('/information')
+def information():
     return render_template('information.html')
     
+@app.route('/donate', methods =['GET', 'POST'])
+def donate():
+	error=''
+	if request.method == 'POST':
+		email = request.form['email']
+		fName = request.form['fname']
+		lName = request.form['lname']
+		bDay = request.form['bDay']
+		sex = request.form['sex']
+		phone = request.form['phone']
+		bloodType = request.form['bloodType']
+		donatedBefore = request.form['donatedBefore']
+		city = request.form['city']
+		lastReaction = request.form['lastReaction']
+		user = {'email': email, 'fullName': fName, 'lastName': lName, 'birthday': bDay, 'sex': sex, 'bloodType':bloodType, 'donatedBefore': donatedBefore, 'city': city, 'phone':phone, 'lastReaction':lastReaction}
+		db.child('Donors').set(user)
+		error ='success'
+	return render_template('form.html', error = error)
 if __name__ == '__main__':
-	app.run()
+	app.run(debug =True)
